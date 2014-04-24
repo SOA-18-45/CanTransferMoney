852956e8aaea43d13b50a9fb3c7db364328aa542
krzysiug <krzysiekg18@gmail.com>
2014-04-24 15:36:05 +0200

    dodany szkielet serwisu

--- ICanTransferMoney/ICanTransferMoney/Program.cs
+++ ICanTransferMoney/ICanTransferMoney/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
+
 
 namespace ICanTransferMoney
 {
@@ -11,4 +13,26 @@ namespace ICanTransferMoney
         {
         }
     }
+
+    [ServiceContract]
+    public interface ICanTransferMoney
+    {
+        [OperationContract]
+        int transferMoney(long accountNumber1, long accountNumber2, decimal value);
+    }
+
+    private class TransferMoney : ICanTransferMoney
+    {
+        int transferMoney(long accountNumber1, long accountNumber2, decimal value)
+        {
+            //zgloszenie sie do servicu IAccountRepository po struktury AccountDetails dla accountNumber1 i 2.
+            //sprawdzenie istnienia obydwu kont i dostępnych środków na 1.
+            //skorzystanie z kontraktu zmieniającego stan kont - wywołanie go na obydwu accountach
+            //zapis do bazy historii transakcji -- account1, account2, value, date
+            
+            return 1;
+        }
+    }
+
+
 }
